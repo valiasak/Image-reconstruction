bcd28ced95a68de29241bb25e0621fe174204b5d
Evangelia Sakkoula <44947853+valiasak@users.noreply.github.com>
2018-11-12 12:25:29 +0100

    Update Image_Reconstruction.m

--- Image_Reconstruction.m
+++ Image_Reconstruction.m
@@ -1,8 +1,10 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright 2018 Evangelia Sakkoula
+% Unauthorized copying of this file, via any medium is strictly prohibited
 % Name: Image_Reconstruction.m
 % Author: Evangelia Sakkoula
 % Email: E.Sakkoula@science.ru.nl
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all 
 close all
