52d3033fadbe517aabc2603b28ea121b087750b7
Evangelia Sakkoula <44947853+valiasak@users.noreply.github.com>
2018-11-12 15:34:51 +0100

    Update colormapRGBmatrices.m

--- colormapRGBmatrices.m
+++ colormapRGBmatrices.m
@@ -1,6 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Copyright 2018 Evangelia Sakkoula
-% Unauthorized copying of this file, via any medium is strictly prohibited
 % Name: colormapRGBmatrices.m
 % Author: Evangelia Sakkoula
 % Email: E.Sakkoula@science.ru.nl
